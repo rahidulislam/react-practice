a1a5bf86dc4e99dd82f6a240ba64f80b541658af
Rahidul Islam <33887458+rahidulislam@users.noreply.github.com>
2023-08-07 10:02:11 +0600

    useeffect updated

--- src/components/DataFetch/DataFetch.jsx
+++ src/components/DataFetch/DataFetch.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const DataFetch = () => {
-    const [todos, setTodos] = useState(null)
+    const [todos, setTodos] = useState([]);
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos/).then(response => response.json()).then(data => {
-            console.log()
-        })
-    })
-  return (
-    <div>DataFetch</div>
-  )
-}
+        fetch("https://jsonplaceholder.typicode.com/todos/")
+            .then((response) => response.json())
+            .then((data) => {
+                setTodos(data);
+                console.log(data);
+            });
+    }, []);
+    return (
+        <div>
+            {todos &&
+                todos.map((todo) => {
+                    return <p key={todo.id}>{todo.title}</p>;
+                })}
+        </div>
+    );
+};
 
-export default DataFetch
+export default DataFetch;
