dbc2e563333f928488a5d559f44814b1eabd0a83
Rahidul Islam <rahiseli@outlook.com>
2023-08-07 12:08:13 +0600

    datafech updated

--- src/components/DataFetch/DataFetch.jsx
+++ src/components/DataFetch/DataFetch.jsx
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from "react";
 
+const loadingMessage = <p>Loading...</p>;
 const DataFetch = () => {
     const [todos, setTodos] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/todos/")
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error("Something went wrong");
+                }
+                return response.json();
+            })
             .then((data) => {
                 setTodos(data);
-                console.log(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                setError(error.message);
             });
     }, []);
+
+    const todoElement =
+        todos &&
+        todos.map((todo) => {
+            return <p key={todo.id}>{todo.title}</p>;
+        });
     return (
         <div>
-            {todos &&
-                todos.map((todo) => {
-                    return <p key={todo.id}>{todo.title}</p>;
-                })}
+            <h1>Todos</h1>
+            {error && <p>{error}</p>}
+            {isLoading && loadingMessage}
+            {todoElement}
         </div>
     );
 };
